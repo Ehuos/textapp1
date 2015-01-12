6e7088b48f23e4bc8aac3247c6e9d27c641a5379
小白 <suixu@jxspy.com>
2015-01-12 15:02:42 +0800

    first commit

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/ant-build/AndroidManifest.xml : \
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/ant-build/CordovaApp.ap_ \
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/ant-build/classes.dex \
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/CordovaLib/ant-build/classes.jar \
