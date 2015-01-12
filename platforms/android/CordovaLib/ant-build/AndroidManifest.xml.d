755f2b76798bf6c15f27f33b4437076b5d4092bd
小白 <suixu@jxspy.com>
2015-01-12 15:15:16 +0800

    first commit

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/jxteam/Work/Go/src/BOTAO/mobile/platforms/android/CordovaLib/AndroidManifest.xml \
